1cc3f0386dcee423a0bd1e7e3e84031ea7fee7e3
Kennedy Opoku Asare <opokuasarekennedy@gmail.com>
2017-05-02 15:03:58 +0300

    Implemented Login Backend

--- src/webClient/static/scripts/bloodalert.js
+++ src/webClient/static/scripts/bloodalert.js
@@ -1,3 +1,4 @@
+
 bloodTypes = []
 DEFUALT_DATA_TYPE = "json"
 
@@ -21,12 +22,63 @@ function loadBloodTypes(event) {
         console.log(data)
     });
 }
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+}
 function handleLoginDonorButton(event) {
     var $form = $(this).closest("form");
     console.log("submit")
     $data = serializeFormTemplate($form)
     console.log($data)
+    return $.ajax({
+        url: "/bloodalert/donors/",
+        dataType: DEFUALT_DATA_TYPE
+    }).done(function (data, textStatus, jqXHR) {
+        
+        for(var i=0;i<data.items.length;i++){
+            item=data.items[i];
+            if(item.email==$data.email){
+                document.cookie()
+                console.log(item);
+                window.loggedInUser=item;
+
+                window.open("http://"+window.location.host+ "/web/profile",)
+                window.loggedInUser=item;
+            }
+        }
+
+        console.log(data)
+    });
 }
 
 function handleDonorRegistration(event) {
